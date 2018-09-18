c493dfc364b045daedd5311c2f4c569f82074ad2
sam wensko <samwensko@gmail.com>
2018-09-18 19:56:44 -0400

    chapter 3 challenge problems

--- challenge/3-19.cpp
+++ challenge/3-19.cpp
@@ -14,7 +14,7 @@ int main() {
 	cout << "Enter the annual interest rate: ";
 	
 	cin >> interestRate;
-	interestRate = interestRate / 12;
+	interestRate = (interestRate/100) / 12;
 
 	cout << "Enter the number of payments  : ";
 	cin >> numPayments;
@@ -22,19 +22,17 @@ int main() {
 	cout << "Enter the amount of the loan  : ";
 	cin >> loanAmount;
 
-
-	// monthly payment = ( (interestRate* (1+interestRate)^2) / ((1+interestRate)^2 - 1) ) * loanAmount
 	monthlyPayment = ( (interestRate * pow((1+interestRate),numPayments)) / (pow((1+interestRate),numPayments) - 1)) * loanAmount;
 
 	paidBack = monthlyPayment * numPayments;
 	interestPaid = paidBack - loanAmount;
 
-	cout << "Loan Amount          : " << loanAmount << endl;
-	cout << "Monthly Interest Rate: " << interestRate << endl;
-	cout << "Number of Payments   : " << numPayments << endl;
-	cout << "Monthly Payment      : " << monthlyPayment << endl;
-	cout << "Amount Paid Back     : " << paidBack << endl;
-	cout << "Interest Paid        : " << interestPaid << endl;
+	cout << "Loan Amount          : $" << setw(7) << loanAmount << endl;
+	cout << "Monthly Interest Rate:  " << setw(6) <<interestRate*100 << "%" << endl;
+	cout << "Number of Payments   :  " << setw(7) <<numPayments << endl;
+	cout << "Monthly Payment      : $" << setw(7) <<monthlyPayment << endl;
+	cout << "Amount Paid Back     : $" << setw(7) <<paidBack << endl;
+	cout << "Interest Paid        : $" << setw(7) <<interestPaid << endl;
 
 	return 0;
 }
